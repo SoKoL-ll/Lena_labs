9e739a296f2746702470ba1a419978dc3f192cae
SoKoL <sokolov-19.07@yandex.ru>
2020-11-30 00:07:57 +0300

    Update main.cpp
    fixed 2.0

--- c++/first_lab/7/main.cpp
+++ c++/first_lab/7/main.cpp
@@ -3,34 +3,20 @@
 
 using namespace std;
 const double eps = 1.e-5;
-double res = 1.0;
 long long x;
 
-long long fact(long long a) {
-    long long result = 1;
-    for (int i = 1; i <= a; i++) {
-        result *= i;
-    }
-    return result;
-}
-
-long long step(long long a, long long b) {
-    long long ans = a;
-    for (int i = 0; i < b; i++) {
-        ans *= a;
-    }
-    return ans;
-}
-
-double rec(double ans, int n) {
+double ans = 1;
+void rec(double now, int n) {
     if (ans > eps) {
-        return (rec(ans + (double)step(-1, n) * (double)step(x, 2 * n)/(double)fact(2*n), n + 1));
+        now *= -((double) (x * x) / (2 * n * (2 * (n - 1))));
+        ans += now;
+        rec(now, n + 1);
     }
-    return ans;
 }
 int main() {
     cin >> x;
-
-    cout << fixed << setprecision (5) << rec(1.0, 1);
+    ans -= (double)(x * x) / 2;
+    rec(ans, 2);
+    cout << fixed << setprecision(5) << ans << endl;
     return 0;
 }
